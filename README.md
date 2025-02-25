1cff96256e683c657a6798675532157fc2ce41a1
Rushikesh Kate <rushikeshkate8@gmail.com>
2025-02-25 21:54:28 +0530

    README file added

---
+++ README.md
@@ -0,0 +1,10 @@
+Tasks App - View, Add, Update and Delete your Day to Day Tasks. 
+Tasks will synced with cloud. 
+
+Tech Used:
+- Jetpack Compose
+- Retrofit
+- Room Database
+- Dependency Injection
+- MVVM
+- Junit, Espresso and Mockito
--- app/src/main/java/com/rushikesh/tasks/ui/screens/TaskListScreen.kt
+++ app/src/main/java/com/rushikesh/tasks/ui/screens/TaskListScreen.kt
@@ -1,28 +1,36 @@
 package com.rushikesh.tasks.ui.screens
 
-import android.R.attr.text
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.rushikesh.tasks.data.model.Task
 import com.rushikesh.tasks.ui.viewmodels.TaskListViewModel
 
 @Composable
-fun TaskListScreen(viewModel: TaskListViewModel = viewModel()) {
+fun TaskListScreen(viewModel: TaskListViewModel = viewModel(), padding: PaddingValues) {
     val tasks = viewModel.tasks.collectAsState()
-
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(padding)) {
         items(tasks.value) { task ->
-            Text(task.title, modifier = Modifier.clickable {
-                viewModel.deleteTask(task.id!!)
-            })
+            TaskItem(task, viewModel)
         }
+    }
+}
+
+@Composable
+fun TaskItem(task: Task, viewModel: TaskListViewModel) {
+    Row {
+        Text(task.title, modifier = Modifier.clickable {
+            viewModel.deleteTask(task.id!!)
+        })
+        Checkbox(task.isCompleted, onCheckedChange =  {})
     }
 }
